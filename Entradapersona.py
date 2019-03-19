54e623c8d16a4fe43f41c32a8d0ab58fd6368503
Guillermo <memocardost@gmail.com>
2019-03-19 16:33:23 -0600

    Entradas
    Entradas

---
+++ Entradapersona.py
@@ -0,0 +1,10 @@
+'''nombre = input("Ingresar nombre: ")
+apellido = input("Ingresar su apellido: ")
+edad = input("ingresa su edad: ")
+
+print(f"Bienvenido {nombre} {apellido}, su edad es: {edad}")'''#la f se utiliza para llamar a las variables
+
+numero1 = int(input("ingrese un numero: "))
+numero2 = int(input("ingrese otro numero: "))
+resultado = numero1 + numero2
+print(f"El resultado es: {resultado}")
