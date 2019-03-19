ef2d1553eb8a8c663da7f9d45d9073a72f673621
Guillermo <memocardost@gmail.com>
2019-03-19 16:53:58 -0600

    Condicionales
    Ver a que corresponde el cumplimiento de las variables

---
+++ condicionales.py
@@ -0,0 +1,11 @@
+a = -7
+
+
+if(a>10):#Si se cumple
+	print("a es mayor a 10")
+elif(a<0): #Si no se cumple el anterior, pero se cumple enta
+	print("en numero es negativo")
+
+else: #Si no se cumple nada
+	print("el numero es menor a 10")
+
