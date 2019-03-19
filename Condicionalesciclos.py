d6f318f734692451c4777a58f620170feb1e423b
Guillermo <memocardost@gmail.com>
2019-03-19 17:59:17 -0600

    For
    El otro tipo de ciclo

--- Condicionalesciclos.py
+++ Condicionalesciclos.py
@@ -1,7 +1,16 @@
 #Ciclo while
-a=0
+'''a=0
 b=100
 
 while(a<b):
 	a=a+1
-	print(a)
+	print(a)'''
+
+	
+#Ciclo for
+a=0
+b=100
+a=a+1
+
+for x in range(1, 101, 2): # el 1° (donde espieza) 2°dodne termina 3°(de cuanto en cuanto)
+	print(x)
